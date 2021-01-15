3285bb8e5e37495ab8b98c2f0e2040ca15fdbc5c
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-15 19:34:56 +0100

    Toyed with Fortify

--- config/fortify.php
+++ config/fortify.php
@@ -133,13 +133,13 @@ return [
 
     'features' => [
         Features::registration(),
-        Features::resetPasswords(),
-        // Features::emailVerification(),
-        Features::updateProfileInformation(),
-        Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirmPassword' => true,
-        ]),
+        //Features::resetPasswords(),
+//         Features::emailVerification(),
+//        Features::updateProfileInformation(),
+//        Features::updatePasswords(),
+//        Features::twoFactorAuthentication([
+//            'confirmPassword' => true,
+//        ]),
     ],
 
 ];
