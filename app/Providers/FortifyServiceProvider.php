9ad401fc4708b71f9729791a723db8b7928ff170
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-16 15:59:01 +0100

    Password Reset Functionality Completed

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -52,5 +52,8 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::requestPasswordResetLinkView(function(){
             return view('auth.forgot-password');
         });
+        Fortify::resetPasswordView(function($request){
+            return view('auth.reset-password', ['request' => $request]);
+        });
     }
 }
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -12,17 +12,13 @@
                         <div class="brand-wrapper">
                             <img src="/img/logo.png" alt="logo" class="logo">
                         </div>
-                        <p class="login-card-description">Sign into your account</p>
-                        @if (session('status'))
-                        <div class="alert alert-success" role='alert'>
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                        <form method="POST" action="{{ route('login') }}">
+                        <p class="login-card-description">Password Reset</p>
+                        <form method="POST" action="{{ route('password.update') }}">
                             @csrf
+                            <input type="hidden" name="token" value="{{ $request->route('token') }}">
                             <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email" placeholder="Email address">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $request->email }}"  autocomplete="email">
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -31,17 +27,20 @@
                             </div>
                             <div class="form-group mb-4">
                                 <label for="password" class="sr-only">Password</label>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  autocomplete="new-password" placeholder="Password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                            <input name="login" id="login" class="btn btn-block login-btn mb-4" type="submit" value="Login">
+                            <div class="form-group mb-4">
+                                <label for="password" class="sr-only">Password</label>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm password">
+                            </div>
+                            <input name="login" id="login" class="btn btn-block login-btn mb-4" type="submit" value="Update">
                         </form>
-                        <a href="{{ route('password.request') }}" class="forgot-password-link">Forgot password?</a>
-                        <p class="login-card-footer-text">Don't have an account? <a href="{{ route('register') }}" class="text-reset">Register here</a></p>
+                        <p class="login-card-footer-text">Not anymore? <a href="{{ route('register') }}" class="text-info text-reset">Sign up</a> or <a href="{{ route('login') }}" class="text-reset">Sign in</a></p>
                         <nav class="login-card-footer-nav">
                             <a href="#!">Terms of use.</a>
                             <a href="#!">Privacy policy</a>
