71f0a150f5e9a8d8d005c1e3ac0c02ac61773995
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-17 13:13:16 +0100

    Recovery Code 2FA

--- resources/views/auth/two-factor-challenge.blade.php
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -26,6 +26,20 @@
                             </div>
                             <input name="login" id="login" class="btn btn-block login-btn mb-4" type="submit" value="Login">
                         </form>
+                        <p>Ener your recovery code</p>
+                        <form method="POST" action="{{ url('/two-factor-challenge') }}">
+                            @csrf
+                            <div class="form-group mb-4">
+                                <label for="recovery_code" class="sr-only">Code</label>
+                                <input id="recovery_code" type="text" class="form-control @error('recovery_code') is-invalid @enderror" name="recovery_code" >
+                                @error('recovery_code')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <input name="login" id="login" class="btn btn-block login-btn mb-4" type="submit" value="Login">
+                        </form>
                     </div>
                 </div>
             </div>
