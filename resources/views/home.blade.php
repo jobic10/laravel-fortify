9d595d272092c613c0dde329136d3715e44ddf5e
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-17 12:37:13 +0100

    Displaying Status

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,11 @@
             <div class="row no-gutters">
                 <div class="col-md-12">
                     <div class="card-body">
-
+                        @if (!auth()->user()->two_factor_secret)
+                            <p class="alert alert-warning">You are yet to enable 2fa</p>
+                            @else
+                            <p class="alert alert-success">2fa is live on this account.</p>
+                        @endif
                     </div>
                 </div>
             </div>
