114c7d6fbeb673053bd029a56029441ea8e3926e
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-20 16:08:45 +0100

    Add column

--- database/migrations/2021_01_20_150821_add_username_to_users_table.php
+++ database/migrations/2021_01_20_150821_add_username_to_users_table.php
@@ -14,7 +14,7 @@ class AddUsernameToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('username');
         });
     }
 
