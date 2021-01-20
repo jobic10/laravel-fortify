ffa21ef7dd2b682c1a7d2f92f79899a5b178cf49
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-20 16:10:43 +0100

    Added username to registration field

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,6 +25,15 @@
                                     </span>
                             @enderror
                             <div class="form-group">
+                                <label for="username" class="sr-only">Username</label>
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus placeholder="Username">
+                            </div>
+                            @error('username')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                            <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email address">
                                 @error('email')
