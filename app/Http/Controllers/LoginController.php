1273a6e13ba28b2a1f451489a0e230641ce9f5cc
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-20 17:59:19 +0100

    Completed response from Github

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -15,9 +15,18 @@ class LoginController extends Controller
 
     public function githubRedirect(){
         //Get Oauth back from github authentication
-        $user = Socialite::driver('github')->user();
+        $github = Socialite::driver('github')->user();
         //if this user does not exist, add
         //If they do, get the model
         // Either way, authenticate the user and redirect
+        $user = User::firstOrCreate([
+            'email' => $github->email
+        ],[
+            'name' => $github->name,
+            'password' => '12345678',
+            'username' => mt_rand(1,22222222)
+        ]);
+        Auth::login($user, true);
+        return redirect('/home');
     }
 }
