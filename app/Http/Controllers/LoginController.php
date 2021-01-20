aade59b10d0d940f1a0460b08add8e40ee8c5b81
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-20 17:37:03 +0100

    Comment

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public function github(){
-
+        //Send User's Request to GitHub
     }
 
     public function githubRedirect(){
