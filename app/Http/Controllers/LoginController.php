229ba6ada57d15603d1d05fc5b135b8a71ae5eb2
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-20 17:50:55 +0100

    Sending user to github

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Auth;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     public function github(){
         //Send User's Request to GitHub
+        return Socialite::driver('github')->redirect();
     }
 
     public function githubRedirect(){
