e05e3c4ba0a210c1a00479c7af836b61e4e73982
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-24 22:25:40 +0100

    Implemented Middleware

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -14,8 +14,11 @@ class CheckRole
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next, string $role)
     {
+        if ($role == 'admin' && auth()->user()->role_id != 1) abort(403);
+        if ($role == 'student' && auth()->user()->role_id != 2) abort(403);
+        if ($role == 'staff' && auth()->user()->role_id != 3) abort(403);
         return $next($request);
     }
 }
