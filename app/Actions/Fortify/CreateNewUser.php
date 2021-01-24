23055b78ed75852071b901f69c85cca38e06cf3a
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-24 21:15:24 +0100

    Validating Role

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -30,7 +30,7 @@ class CreateNewUser implements CreatesNewUsers
                 'max:255',
                 Rule::unique(User::class),
             ],
-            // 'role_id' => ['required', 'int', 'min:1'],
+            'register_as' => ['required', 'integer'],
             'password' => $this->passwordRules(),
         ])->validate();
 $data = [
