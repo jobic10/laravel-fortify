5deb1d787128a68532b967e140c8e16c2e507e9e
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-24 22:02:37 +0100

    Added name to roles

--- database/migrations/2021_01_24_210003_add_name_to_roles_table.php
+++ database/migrations/2021_01_24_210003_add_name_to_roles_table.php
@@ -14,7 +14,7 @@ class AddNameToRolesTable extends Migration
     public function up()
     {
         Schema::table('roles', function (Blueprint $table) {
-            //
+            $table->string('name');
         });
     }
 
